359ebc4239855f5f484b2f32ffec35c0c2c5a50b
IgorVillar06 <99916805+IgorVillar06@users.noreply.github.com>
2022-09-01 16:45:02 -0300

    Upload

--- NPS.py
+++ NPS.py
@@ -6,7 +6,24 @@ import numpy as np
 database = pd.read_csv("report.csv", sep=';', index_col=0)
 database = database.drop_duplicates()
 
-#agente1 = database [database['Agente'] == 'Igor Villar']
+avaliacoes = database [(database['Nota'] >= 1)]
+contar_avaliacoes = avaliacoes ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+total_avaliacoes = contar_avaliacoes.sum()
+
+promotor = database [(database['Nota'] >= 9)]
+contar_promotor = promotor ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+total_promotores = contar_promotor.sum()
+
+detrator = database [(database['Nota'] >= 0) & (database['Nota'] <= 6)] 
+contar_detrator = detrator ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+total_detrator = contar_detrator.sum()
+
+nps_time = ((total_promotores - total_detrator) / total_avaliacoes) * 100
+nps_time = int(nps_time['Nota'])
+print("NPS do time: ", nps_time)
+
+database = pd.read_csv("report.csv", sep=';', index_col=0)
+database = database.drop_duplicates()
 
 avaliacoes = database [(database['Nota'] >= 1) & (database['Agente'] == 'Igor Villar')]
 contar_avaliacoes = avaliacoes ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
@@ -20,5 +37,6 @@ detrator = database [(database['Nota'] >= 0) & (database['Nota'] <= 6) & (databa
 contar_detrator = detrator ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
 total_detrator = contar_detrator.sum()
 
-nps = [((total_promotores - total_detrator) / total_avaliacoes) * 100]
-print("o nps de Igor é: ", nps)
+nps_igor = ((total_promotores - total_detrator) / total_avaliacoes) * 100
+nps_igor = int(nps_igor['Nota'])
+print("NPS do Igor Villar: ", nps_igor)
