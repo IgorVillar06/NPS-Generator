4c1d4a33efa65aaabcd62ecfda1626f896b7784e
IgorVillar06 <99916805+IgorVillar06@users.noreply.github.com>
2022-09-02 11:01:53 -0300

    updaate

--- NPS.py
+++ NPS.py
@@ -1,6 +1,47 @@
 import pandas as pd
 import numpy as np
 
+database = pd.read_csv("report5hub.csv", sep=',', index_col=0)
+database = database.drop_duplicates().dropna()
+
+avaliacoes = database [(database['Nota'] >= 10)]
+contar_avaliacoes = avaliacoes ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+total_avaliacoes = contar_avaliacoes.sum()
+
+promotor = database [(database['Nota'] >= 90)]
+contar_promotor = promotor ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+total_promotores = contar_promotor.sum()
+
+detrator = database [(database['Nota'] >= 0) & (database['Nota'] <= 60)] 
+contar_detrator = detrator ['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+total_detrator = contar_detrator.sum()
+
+nps_time = ((total_promotores - total_detrator) / total_avaliacoes) * 100
+nps_time = int(nps_time['Nota'])
+print("NPS do time (5Hub):", nps_time)
+
+print("==========================================================================================")
+
+for Agente in database['Agente'].drop_duplicates():
+    avaliacoes = database[(database['Nota'] >= 10) & (database['Agente'] == Agente)]
+    contar_avaliacoes = avaliacoes['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+    total_avaliacoes = contar_avaliacoes.sum()
+
+    promotor = database[(database['Nota'] >= 90) & (database['Agente'] == Agente)]
+    contar_promotor = promotor['Nota'].value_counts(dropna=False, ascending=False). to_frame()
+    total_promotores = contar_promotor.sum()
+
+    detrator = database[(database['Nota'] >= 0) & (database['Nota'] <= 60) & (database['Agente'] == Agente)]
+    contar_detrator = detrator['Nota'].value_counts(
+    dropna=False, ascending=False). to_frame()
+    total_detrator = contar_detrator.sum()
+
+    nps_time = (((total_promotores - total_detrator) / total_avaliacoes) * 100)
+    nps_time = int(nps_time['Nota'])
+    print("NPS de", Agente, "(5Hub):", nps_time)
+
+print("==========================================================================================")    
+    
 database = pd.read_csv("report.csv", sep=';', index_col=0)
 database = database.drop_duplicates().dropna()
 
@@ -18,7 +59,9 @@ total_detrator = contar_detrator.sum()
 
 nps_time = ((total_promotores - total_detrator) / total_avaliacoes) * 100
 nps_time = int(nps_time['Nota'])
-print("NPS do time:", nps_time)
+print("NPS do time (Native):", nps_time)
+
+print("==========================================================================================")
 
 for Agente in database['Agente'].drop_duplicates():
     avaliacoes = database[(database['Nota'] >= 1) & (database['Agente'] == Agente)]
@@ -36,4 +79,4 @@ for Agente in database['Agente'].drop_duplicates():
 
     nps_time = (((total_promotores - total_detrator) / total_avaliacoes) * 100)
     nps_time = int(nps_time['Nota'])
-    print("NPS de", Agente, ":", nps_time)
+    print("NPS de", Agente, "(Native):", nps_time)
